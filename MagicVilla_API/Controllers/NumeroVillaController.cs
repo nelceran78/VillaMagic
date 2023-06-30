2fe5e678e18ce2cd8c6b2d77994dea05cbabe924
Nelson Cerdas <nelson.cerdas@outlook.com>
2023-06-30 09:25:27 -0600

    Creacion Modelo NumeroVilla

---
+++ MagicVilla_API/Controllers/NumeroVillaController.cs
@@ -0,0 +1,222 @@
+﻿using AutoMapper;
+using MagicVilla_API.Datos;
+using MagicVilla_API.Modelos;
+using MagicVilla_API.Modelos.Dto;
+using MagicVilla_API.Repositorio.IRepositorio;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Net;
+
+namespace MagicVilla_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NumeroVillaController : ControllerBase
+    {
+        private readonly ILogger<NumeroVillaController> _logger;
+        private readonly IVillaRepositorio _villaRepo;
+        private readonly INumeroVillaRepositorio _numeroRepo;
+        private readonly IMapper _mapper;
+        protected APIResponse _response;
+
+        public NumeroVillaController(ILogger<NumeroVillaController> logger, 
+                    IVillaRepositorio villaRepo,INumeroVillaRepositorio numeroRepo ,IMapper mapper)
+        {
+            _logger = logger;
+            _villaRepo = villaRepo;
+            _numeroRepo = numeroRepo;
+            _mapper = mapper;
+            _response = new();
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<ActionResult<APIResponse>> GetNumeroVillas() 
+        {
+            try
+            {
+                _logger.LogInformation("Obtener todas la Villas");
+
+                IEnumerable<NumeroVilla> numerovillaList = await _numeroRepo.ObtenerTodos();
+                _response.Resultado = _mapper.Map<IEnumerable<NumeroVillaDto>>(numerovillaList);
+                _response.statusCode = HttpStatusCode.OK;
+
+                return Ok(_response);
+            }
+            catch (Exception ex)
+            {
+
+                _response.IsExitoso = false;
+                _response.ErrorMensages = new List<string>() { ex.ToString() };
+            }
+            return _response;
+        }
+
+        [HttpGet("{id:int}", Name ="GetNumeroVilla")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<APIResponse>> GetNumeroVilla(int id) 
+        {
+            try
+            {
+                if (id == 0)
+                {
+                    _logger.LogError("Error al traer el numero de villa con el id " + id);
+                    _response.IsExitoso = false;
+                    _response.statusCode = HttpStatusCode.BadRequest;
+                    return BadRequest(_response);
+                }
+
+                var numerovilla = await _numeroRepo.Obtener(v => v.VillaNo == id);
+                if (numerovilla == null)
+                {
+                    _logger.LogError("Error al traer el numero de villa con el id " + id);
+                    _response.IsExitoso = false;
+                    _response.statusCode = HttpStatusCode.NotFound;
+                    return NotFound(_response);
+                }
+
+                _response.Resultado = _mapper.Map<NumeroVillaDto>(numerovilla);
+                _response.statusCode = HttpStatusCode.OK;
+                return Ok(_response);
+            }
+            catch (Exception ex)
+            {
+
+                _response.IsExitoso = false;
+                _response.ErrorMensages = new List<string>() { ex.ToString() };
+            }
+
+            return _response;
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<APIResponse>> CrearNumeroVilla([FromBody] NumeroVillaCreateDto createDto)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest(ModelState);
+                }
+
+                if (await _numeroRepo.Obtener(v => v.VillaNo == createDto.VillaNo) != null)
+                {
+                    ModelState.AddModelError("NombreExiste", "El numero de villa ya Existe");
+                    return BadRequest(ModelState);
+                }
+
+                if(await _villaRepo.Obtener(v=>v.Id==createDto.VillaId) == null)
+                {
+                    ModelState.AddModelError("ClaveForanea", "El Id de la villa no existe");
+                    return BadRequest(ModelState);
+                }
+
+
+                if (createDto == null)
+                {
+                    return BadRequest(createDto);
+                }
+
+                NumeroVilla modelo = _mapper.Map<NumeroVilla>(createDto);
+
+                modelo.FechaCreacion = DateTime.Now;
+                modelo.FechaActualizacion = DateTime.Now;
+                await _numeroRepo.Crear(modelo);
+                _response.Resultado = modelo;
+                _response.statusCode = HttpStatusCode.Created;
+
+                return CreatedAtRoute("GetVilla", new { id = modelo.VillaNo }, _response);
+            }
+            catch (Exception ex)
+            {
+
+                _response.IsExitoso = false;
+                _response.ErrorMensages = new List<string>() { ex.ToString() };
+            }
+            return _response;          
+        }
+
+        [HttpDelete("{id:int}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> DeleteNumeroVilla(int id)
+        {
+            try
+            {
+                if (id == 0)
+                {
+                    _response.IsExitoso = false;
+                    _response.statusCode = HttpStatusCode.BadRequest;
+                    return BadRequest(_response);
+                }
+
+                var numerovilla = await _numeroRepo.Obtener(v => v.VillaNo == id);
+                if (numerovilla == null)
+                {
+                    _response.IsExitoso = false;
+                    _response.statusCode = HttpStatusCode.NotFound;
+                    return NotFound(_response);
+                }
+
+                await _numeroRepo.Remover(numerovilla);
+
+                _response.statusCode = HttpStatusCode.NoContent;
+
+                return Ok(_response);
+            }
+            catch (Exception ex)
+            {
+
+                _response.IsExitoso = false;
+                _response.ErrorMensages = new List<string>() { ex.ToString() };
+            }
+            return BadRequest(_response);           
+        }
+
+        [HttpPut("{id:int}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> UpdateNumeroVilla(int id, [FromBody] NumeroVillaUpdateDto updateDto)
+        {
+            try
+            {
+                if (updateDto == null || id != updateDto.VillaNo)
+                {
+                    _logger.LogError("Error al traer la villa con el id " + id);
+                    _response.IsExitoso = false;
+                    _response.statusCode = HttpStatusCode.BadRequest;
+                    return BadRequest(_response);
+                }
+
+                if(await _villaRepo.Obtener(v=>v.Id == updateDto.VillaId) == null)
+                {
+                    ModelState.AddModelError("ClaveForanea", "El Id de la villa no existe");
+                    return BadRequest(ModelState);
+                }
+
+                NumeroVilla modelo = _mapper.Map<NumeroVilla>(updateDto);
+
+
+                await _numeroRepo.Actualizar(modelo);
+                _response.statusCode = HttpStatusCode.NoContent;
+
+                return Ok(_response);
+            }
+            catch (Exception ex)
+            {
+                _response.IsExitoso = false;
+                _response.ErrorMensages = new List<string>() { ex.ToString() };
+            }
+            return Ok(_response);            
+        }
+                
+    }
+}
---
+++ MagicVilla_API/Migrations/20230630144916_AgregarBDNumeroVilla.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MagicVilla_API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AgregarBDNumeroVilla : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "NumeroVillas",
+                columns: table => new
+                {
+                    VillaNo = table.Column<int>(type: "int", nullable: false),
+                    VillaId = table.Column<int>(type: "int", nullable: false),
+                    DetalleEspecial = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    FechaCreacion = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    FechaActualizacion = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_NumeroVillas", x => x.VillaNo);
+                    table.ForeignKey(
+                        name: "FK_NumeroVillas_Villas_VillaId",
+                        column: x => x.VillaId,
+                        principalTable: "Villas",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "FechaActualizacion", "FechaCreacion" },
+                values: new object[] { new DateTime(2023, 6, 30, 8, 49, 15, 928, DateTimeKind.Local).AddTicks(884), new DateTime(2023, 6, 30, 8, 49, 15, 928, DateTimeKind.Local).AddTicks(863) });
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "FechaActualizacion", "FechaCreacion" },
+                values: new object[] { new DateTime(2023, 6, 30, 8, 49, 15, 928, DateTimeKind.Local).AddTicks(888), new DateTime(2023, 6, 30, 8, 49, 15, 928, DateTimeKind.Local).AddTicks(887) });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_NumeroVillas_VillaId",
+                table: "NumeroVillas",
+                column: "VillaId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "NumeroVillas");
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "FechaActualizacion", "FechaCreacion" },
+                values: new object[] { new DateTime(2023, 6, 29, 13, 9, 47, 953, DateTimeKind.Local).AddTicks(942), new DateTime(2023, 6, 29, 13, 9, 47, 953, DateTimeKind.Local).AddTicks(922) });
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "FechaActualizacion", "FechaCreacion" },
+                values: new object[] { new DateTime(2023, 6, 29, 13, 9, 47, 953, DateTimeKind.Local).AddTicks(946), new DateTime(2023, 6, 29, 13, 9, 47, 953, DateTimeKind.Local).AddTicks(945) });
+        }
+    }
+}
